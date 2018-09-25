9afc341e2aa7940c3f7fc7f2e4bad82a794cc9ce
wzb_mac <wzb121318@163.com>
2018-09-26 00:07:04 +0800

    cloud spring boot

---
+++ src/main/java/com/scwl/cloud/CloudApplication.java
@@ -0,0 +1,19 @@
+package com.scwl.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring boot 服务启动类
+ *
+ * @author weizhibang
+ * @date 2018/10/31
+ */
+
+@SpringBootApplication
+public class CloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudApplication.class, args);
+    }
+}
